d0f37cdd5a2cea5756279710d6cd2bc1c6940db2
Cesar Castillo <cesar.castillo@leanix.net>
2021-07-20 13:16:22 +0200

    INTG-2370 Add building steps for this action

---
+++ test/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+docker run --rm \
+  -e ARM_SUBSCRIPTION_ID=$ARM_SUBSCRIPTION_ID \
+  -e ARM_TENANT_ID=$ARM_TENANT_ID \
+  -e ARM_CLIENT_ID=$ARM_CLIENT_ID \
+  -e ARM_CLIENT_SECRET=$ARM_CLIENT_SECRET \
+  -e INPUT_ENVIRONMENT=test \
+  -e INPUT_SUBSCRIPTION_FILE="test/integration-api-workspace-created-events.json" \
+  -v $(pwd):/github/workspace \
+  leanixacrpublic.azurecr.io/webhooks-subscription-upsert-action:$1
